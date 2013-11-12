49ac3122e84498a45e84b4a45c23282837666611
Max Ciotti <mdciotti@gmail.com>
2013-11-12 08:54:01 -0500

    Cleared branch

--- compiled_templates/main.js
+++
@@ -1,1 +0,0 @@
-(function(){dust.register("main",body_0);function body_0(chk,ctx){return chk.block(ctx.getBlock("header"),ctx,{},null).write("<div class=\"main-view\">").block(ctx.getBlock("sidebar"),ctx,{},null).block(ctx.getBlock("main_view"),ctx,{},null).write("</div>").block(ctx.getBlock("banner"),ctx,{},null);}return body_0;})();
